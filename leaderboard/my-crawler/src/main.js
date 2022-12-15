baa49d8bab3563e96a5d6c8bcb4e3b76ac24d1cb
Evan <evan.higgins@wpengine.com>
2022-12-15 19:02:49 +0000

    nit: fmt + add let to tags var

--- leaderboard/my-crawler/src/main.js
+++ leaderboard/my-crawler/src/main.js
@@ -8,54 +8,53 @@ import * as puppeteer from 'puppeteer';
 //const {sleep, RequestQueue, Dataset, PuppeteerCrawler, createRequestDebugInfo} = require('crawlee')
 //const {puppeteer} = require('puppeteer')
 
-tags = [
-    "john-390",
-    "marf-776",
-    "verd-230",
-    "aldo-285",
-    "frog-338",
-	"mint-0",
-    "razz-632"
+let tags = [
+  "john-390",
+  "marf-776",
+  "verd-230",
+  "aldo-285",
+  "frog-338",
+  "mint-0",
+  "razz-632"
 ]
 
-      const requestQueue = await RequestQueue.open();
-      for (const player of tags) {
-        await requestQueue.addRequest({ url: `https://slippi.gg/user/${player}` });
+const requestQueue = await RequestQueue.open();
+for (const player of tags) {
+  await requestQueue.addRequest({ url: `https://slippi.gg/user/${player}` });
+}
+
+const allinfo = []
+const crawler = new PuppeteerCrawler({
+  launchContext: {
+    launchOptions: {
+      headless: true,
+    },
+    useChrome: true
+  },
+  requestQueue,
+  requestHandlerTimeoutSecs: 120,
+  navigationTimeoutSecs: 1000,
+  async requestHandler({ page, request, log }) {
+    let links = await page.$$("a")
+    await links[2].click()
+    await page.goBack()
+    await sleep(2000)
+    let containerText = (await page.$eval('.container', e => e.innerText))
+    let infoSplit = containerText.split(`\n`)
+    allinfo.push(`Slippi Tag: ${infoSplit[2]}\nELO: ${parseFloat((infoSplit[6].split(" "))[0])}\n`)
+    fs.writeFile("table.txt", allinfo.join(`\n`), (err) => {
+      if (err)
+        console.log(err);
+      else {
+        console.log("File written successfully\n");
+        console.log("The written has the following contents:");
+        console.log(fs.readFileSync("table.txt", "utf8"));
       }
-     
-      
-      const allinfo = []
-      const crawler = new PuppeteerCrawler({
-          launchContext: {
-              launchOptions: {
-                  headless: true,
-              },
-              useChrome: true
-          },
-          requestQueue,
-          requestHandlerTimeoutSecs: 120,
-          navigationTimeoutSecs: 1000,
-          async requestHandler({ page, request, log }) {
-              let links = await page.$$("a")
-              await links[2].click() 
-              await page.goBack()
-              await sleep(2000)
-              let containerText = (await page.$eval('.container', e=>e.innerText))
-              let infoSplit = containerText.split(`\n`)
-              allinfo.push(`Slippi Tag: ${infoSplit[2]}\nELO: ${parseFloat((infoSplit[6].split(" "))[0])}\n`)
-			  fs.writeFile("table.txt", allinfo.join(`\n`), (err) => {
-                if (err)
-                  console.log(err);
-                else {
-                  console.log("File written successfully\n");
-                  console.log("The written has the following contents:");
-                  console.log(fs.readFileSync("table.txt", "utf8"));
-                }
-              });
-              await Dataset.pushData({"info": infoSplit})
-          },
-          failedRequestHandler({ request, log }) {
-              log.error(`Request ${request.url} failed too many times.`);
-          },
-      });
-      await crawler.run();
+    });
+    await Dataset.pushData({ "info": infoSplit })
+  },
+  failedRequestHandler({ request, log }) {
+    log.error(`Request ${request.url} failed too many times.`);
+  },
+});
+await crawler.run();
