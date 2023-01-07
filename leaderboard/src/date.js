6fb8b2b1e101cfb1e22401be396bd634578b5102
Evan <evan.higgins@wpengine.com>
2023-01-07 23:31:45 +0000

    feat: getTimestamp() func

--- leaderboard/src/date.js
+++ leaderboard/src/date.js
@@ -1,13 +1,6 @@
 function getTimestamp(){
 
-    const date = new Date();
+    const date = document.lastModified;
 
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    let time = date.getHours() + ":" + date.getMinutes();
-
-    let currentTime = `${day}/${month}/${year} ${time}`
-
-    document.getElementById('timestamp').innerHTML = "Leaderboard last updated: " + currentTime;
+    document.getElementById('timestamp').innerHTML = "Leaderboard last updated: " + date;
 }
